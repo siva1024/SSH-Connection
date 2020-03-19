ee6c33ba4c8f7c035c9fa8086a59bd5ee7ad90da
siva1024 <siva.radium@gmail.com>
2020-03-18 23:30:13 -0700

    Updated ssh file

--- sshConnection.sh
+++ sshConnection.sh
@@ -2,3 +2,6 @@ ssh-keygen -t rsa
 ssh user@hostname mkdir -p .ssh
 cat .ssh/id_rsa.pub | ssh user@hostname 'cat >> .ssh/authorized_keys'
 ssh user@hostname "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
+
+# Disable password authentication "/etc/ssh/sshd_config"
+#PasswordAuthentication no
