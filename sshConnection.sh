863b9eb94647f6ae0c2141c31f7ea445f7511acd
siva1024 <31105586+siva1024@users.noreply.github.com>
2021-02-01 11:51:38 -0800

    Update sshConnection.sh

--- sshConnection.sh
+++ sshConnection.sh
@@ -1,4 +1,4 @@
-ssh-keygen -t rsa
+ssh-keygen -t rsa -b 4096
 ssh user@hostname mkdir -p .ssh
 cat .ssh/id_rsa.pub | ssh user@hostname 'cat >> .ssh/authorized_keys'
 ssh user@hostname "chmod 700 .ssh; chmod 640 .ssh/authorized_keys"
